32afba6a963b3cac668278dca1432706831437b5
gihankarunarathne <gckarunarathne@gmail.com>
2019-02-01 17:30:35 +0400

    Implemented retrieve data from k8s metrics-server and render

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ WORKDIR /go/src/app
 RUN apk update && apk add git sqlite gcc g++
 RUN go get -u github.com/kataras/iris 
 RUN go get -u github.com/mattn/go-sqlite3
+RUN go get -u k8s.io/apimachinery/pkg/apis/meta/v1
+RUN go get -u k8s.io/client-go/kubernetes
+RUN go get -u k8s.io/client-go/rest
+RUN go get -u github.com/wcharczuk/go-chart
 COPY ./src .
 
 RUN go get -d -v ./...
--- src/app.go
+++ src/app.go
@@ -4,69 +4,92 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net"
-	"net/http"
 	"time"
 
 	"github.com/kataras/iris"
 	_ "github.com/mattn/go-sqlite3"
+	chart "github.com/wcharczuk/go-chart"
+	util "github.com/wcharczuk/go-chart/util"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 const (
-	database      = "wdias"
-	metricsServer = "http://metrics-server.default.svc.cluster.local"
-	controlLoop   = 3
+	database    = "wdias"
+	controlLoop = 3
 )
 
-// Point : Data Point
-type point struct {
-	Time  string  `json:"time"`
-	Value float64 `json:"value"`
+// PodMetricsList : PodMetricsList
+type PodMetricsList struct {
+	Kind       string `json:"kind"`
+	APIVersion string `json:"apiVersion"`
+	Metadata   struct {
+		SelfLink string `json:"selfLink"`
+	} `json:"metadata"`
+	Items []struct {
+		Metadata struct {
+			Name              string    `json:"name"`
+			Namespace         string    `json:"namespace"`
+			SelfLink          string    `json:"selfLink"`
+			CreationTimestamp time.Time `json:"creationTimestamp"`
+		} `json:"metadata"`
+		Timestamp  time.Time `json:"timestamp"`
+		Window     string    `json:"window"`
+		Containers []struct {
+			Name  string `json:"name"`
+			Usage struct {
+				CPU    string `json:"cpu"`
+				Memory string `json:"memory"`
+			} `json:"usage"`
+		} `json:"containers"`
+	} `json:"items"`
 }
 
-// Timeseries : Timeseries
-type timeseries struct {
-	TimeseriesID   string `json:"timeseriesId"`
-	ModuleID       string `json:"moduleId"`
-	ValueType      string `json:"valueType"`
-	ParameterID    string `json:"parameterId"`
-	LocationID     string `json:"locationId"`
-	TimeseriesType string `json:"timeseriesType"`
-	TimeStepID     string `json:"timeStepId"`
-}
-
-func getTimeseries(timeseriesID string, metadata *timeseries) error {
-	fmt.Println("URL:", fmt.Sprint(metricsServer, "/timeseries/", timeseriesID))
-	response, err := netClient.Get(fmt.Sprint(metricsServer, "/timeseries/", timeseriesID))
+func getMetrics(clientset *kubernetes.Clientset, pods *PodMetricsList) error {
+	data, err := clientset.RESTClient().Get().AbsPath("apis/metrics.k8s.io/v1beta1/pods").DoRaw()
 	if err != nil {
 		return err
 	}
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return err
-	}
-	err = json.Unmarshal(body, &metadata)
+	err = json.Unmarshal(data, &pods)
 	return err
-}
-
-var tr = &http.Transport{
-	MaxIdleConns:       10,
-	IdleConnTimeout:    30 * time.Second,
-	DisableCompression: true,
-	Dial: (&net.Dialer{
-		Timeout: 5 * time.Second,
-	}).Dial,
-	TLSHandshakeTimeout: 5 * time.Second,
-}
-var netClient = &http.Client{
-	Transport: tr,
-	Timeout:   time.Second * 10,
 }
 
 func main() {
 	app := iris.Default()
+
+	app.Get("/metrics", func(ctx iris.Context) {
+		graph := chart.Chart{
+			XAxis: chart.XAxis{
+				Style:        chart.StyleShow(), //enables / displays the x-axis
+				TickPosition: chart.TickPositionBetweenTicks,
+				ValueFormatter: func(v interface{}) string {
+					typed := v.(float64)
+					typedDate := util.Time.FromFloat64(typed)
+					return fmt.Sprintf("%d-%d\n%d", typedDate.Month(), typedDate.Day(), typedDate.Year())
+				},
+			},
+			YAxis: chart.YAxis{
+				Style: chart.StyleShow(), //enables / displays the y-axis
+			},
+			YAxisSecondary: chart.YAxis{
+				Style: chart.StyleShow(), //enables / displays the secondary y-axis
+			},
+			Series: []chart.Series{
+				chart.ContinuousSeries{
+					XValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
+					YValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0},
+				},
+				chart.ContinuousSeries{
+					YAxis:   chart.YAxisSecondary,
+					XValues: []float64{1.1, 2.2, 3.3, 4.4, 5.5},
+					YValues: []float64{50.0, 40.0, 30.0, 20.0, 10.0},
+				},
+			},
+		}
+
+		ctx.Header("Content-Type", "image/png")
+		graph.Render(chart.PNG, ctx)
+	})
 
 	app.Get("/public/hc", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{
@@ -92,6 +115,16 @@ func main() {
 		fmt.Println("Unable to create table. Exit.", err.Error())
 		return
 	}
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 
 	for {
 		time.Sleep(controlLoop * time.Second)
@@ -102,11 +135,21 @@ func main() {
 			continue
 		}
 		defer stmt.Close()
-		_, err = stmt.Exec("adapter-scalar-77d6998d99-qp2zq", "default", "2019-01-30T14:03:00Z")
+		var pods PodMetricsList
+		err = getMetrics(clientset, &pods)
 		if err != nil {
-			fmt.Println("Unable to insert data.", err.Error())
-			// continue
+			fmt.Println("Unable to get metrics.", err.Error())
+			continue
 		}
+		for _, m := range pods.Items {
+			fmt.Println(m.Metadata.Name, m.Metadata.Namespace, m.Timestamp.String())
+			_, err = stmt.Exec(m.Metadata.Name, m.Metadata.Namespace, m.Timestamp)
+			if err != nil {
+				fmt.Println("Unable to insert data.", err.Error())
+				continue
+			}
+		}
+		// Query
 		rows, err := db.Query("SELECT * FROM metrics")
 		if err != nil {
 			fmt.Println("Unable to query data.", err.Error())
@@ -125,5 +168,6 @@ func main() {
 			}
 			fmt.Println("row:", name, namespace, timestamp)
 		}
+		time.Sleep(56 * time.Second)
 	}
 }
