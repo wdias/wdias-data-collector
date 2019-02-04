c28224d7b4eb672d80b3a78141b121ebb4a5bd24
gihankarunarathne <gckarunarathne@gmail.com>
2019-02-04 15:03:00 +0400

    Implemented loading data from the server

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+const dataServer = 'http://analysis.wdias.com'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: [],
+    };
+  }
+  async loadData() {
+    const res = await axios.get(`${dataServer}/metrics`);
+    if(res.status === 200) {
+      const d = res.data;
+      console.log(d, d.length);
+      const chartData = d.map(helmChart => {
+        let col = {
+          name: helmChart.timestamp,
+        };
+        console.log('len: ', helmChart.podsPerHelmChart.length)
+        for (const pod of helmChart.podsPerHelmChart) {
+          console.log(pod)
+          col[pod.helmChart] = pod.noPods;
+        }
+        return col;
+      });
+      console.log(this.state.data)
+      this.setState({ chartData: chartData });
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +47,11 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <button onClick={() => this.loadData()}>Refresh</button>
+        <div>
+          <p>{JSON.stringify(this.state.chartData)}</p>
+        </div>
       </div>
     );
   }
