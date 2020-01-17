ba02402ec614fdd53ce7addfb28fa804e8f25856
gihankarunarathne <gckarunarathne@gmail.com>
2020-01-17 21:23:31 +0400

    Add web build

---
+++ web/build/precache-manifest.3249acd3a4546a8ca29cd609456fe4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f932d3feeeab3b012ca15af339ecc230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fde0d2b6cc2b9f71de0",
+    "url": "/static/css/main.4d493388.chunk.css"
+  },
+  {
+    "revision": "2a0fe197f34cd11ad447",
+    "url": "/static/js/2.b1320cf7.chunk.js"
+  },
+  {
+    "revision": "302156c30e618e6050a190c61d88760e",
+    "url": "/static/js/2.b1320cf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fde0d2b6cc2b9f71de0",
+    "url": "/static/js/main.b111014f.chunk.js"
+  },
+  {
+    "revision": "2eb5ea446c722acffd56",
+    "url": "/static/js/runtime-main.bb040c98.js"
+  }
+]);
